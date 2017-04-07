42febc40a7957a20d539f2e08f53303f507ed031
Chong_Sing_lui <chong_sing_lui@manulifeam.com>
2017-04-07 09:43:13 +0800

    Fix the overflow arguments

--- exec/exec_win.go
+++ exec/exec_win.go
@@ -17,19 +17,20 @@ var (
 )
 
 func init() {
-	shell, err := exec.LookPath("sh")
+	var err error
+	shell, err = exec.LookPath("sh")
 	exists = err == nil
 }
 
-func Command(name string, args ...string) *Cmd {
-	c := &Cmd{exec.Command(shell, "-c", name, args...)}
+func Command(cmd string) *Cmd {
+	c := &Cmd{exec.Command(shell, "-c", cmd)}
 	c.Env = os.Environ()
 	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 
 	if !exists {
-		c.Cmd = exec.Command("cmd", "/C", name, args...)
+		c.Cmd = exec.Command("cmd", "/C", cmd)
 	}
 
 	return c
